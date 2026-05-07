215772dd87ff88a87b0f2d13ea1c72750cf98b4d
Djahidhnf <hnfdjahid@gmail.com>
2026-05-07 11:35:10 +0200

    middleware runtime

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+export const runtime = 'nodejs'
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
